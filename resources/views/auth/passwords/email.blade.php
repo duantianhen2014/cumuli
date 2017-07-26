d6c3c65a343bc0b8637e025c73ae8b6549bf88a9
wangdong <mail@wangdong.io>
2017-07-26 17:12:01 +0800

    增加文档文件

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    {{--右侧内容区域--}}
+    <div
+        data-options="region:'center',border:false,title:'{{ config('app.name') }}',iconCls:'{{ config('app.icon') }}'">
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+        <div class="easyui-dialog" data-options="closed:true" style="width: 100%;max-width: 400px;padding: 30px 36px;">
+            <form>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div>
+                    <input class="easyui-textbox" type="email" name="email" style="width:100%;height:36px;padding:12px"
+                           data-options="required:true,validType:'email',prompt:'请输入邮箱',iconCls:'fa fa-envelope',iconWidth:22">
                 </div>
-            </div>
+            </form>
         </div>
+
     </div>
-</div>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.textbox-icon').css('lineHeight', '34px');
+            $('.fa').css('color', 'black');
+
+            $dialog = $('.easyui-dialog:first');
+            $dialog.dialog({
+                title: '密码重置',
+                iconCls: 'fa fa-lock',
+                closed: false,
+                closable: false,
+                constrain: true,
+                buttons: [{
+                    text: '发送邮件',
+                    iconCls: 'fa fa-send',
+                    handler: function () {
+                        $dialog.find('form:first').form('submit', {
+                            onSubmit: function () {
+                                // 表单验证
+                                let isValid = $(this).form('validate');
+                                if (!isValid) return false;
+
+                                // 表单提交
+                                $.cumuli.request.post('{{ route('password.email') }}', this, function (data) {
+                                    if (data.status == 'error') {
+                                        return $.cumuli.message.show(data.message, 'error');
+                                    }
+                                    window.location.href = '/';
+                                });
+
+                                // 防止触发form提交
+                                return false;
+                            }
+                        });
+                    }
+                }],
+                onOpen: function () {
+                    // 回车提交表单
+                    $dialog.find('form:first').on('keyup', function (event) {
+                        if (event.keyCode == 13) $dialog.dialog('options').buttons[0].handler();
+                    });
+                }
+            });
+        });
+    </script>
 @endsection
