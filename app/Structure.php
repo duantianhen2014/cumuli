a0e32b21f815700acceccc851b9d03c52fa663cf
wangdong <mail@wangdong.io>
2017-08-22 18:20:49 +0800

    增加递归查询

--- app/Structure.php
+++ app/Structure.php
@@ -7,8 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Structure extends Model
 {
     protected $fillable = [
-        'name', 'description',
+        'name', 'description', 'parent_id',
     ];
+
+    // 追加字段
+    protected $appends = [
+        'iconCls'
+    ];
+
+    /**
+     * 自定义图标
+     * @return string
+     */
+    public function getIconClsAttribute()
+    {
+        return 'fa fa-folder-o';
+    }
 
     /**
      * 多对多关联
@@ -19,4 +33,23 @@ class Structure extends Model
     {
         return $this->belongsToMany('App\User', 'structure_users');
     }
+
+    /**
+     * 上下级关联
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function child()
+    {
+        return $this->hasMany('App\Structure', 'parent_id', 'id');
+    }
+
+    /**
+     * 递归查询
+     * @return $this
+     */
+    public function children()
+    {
+        return $this->child()->with('children');
+    }
+
 }
