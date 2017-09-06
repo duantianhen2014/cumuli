39b269963f55ff2fdc4a1226381ea3b68bdcf65c
wangdong <mail@wangdong.io>
2017-09-06 09:56:10 +0800

    调整Cumuli核心为dev版本

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Cumuli\Support\ServiceProvider::class,
     ],
 
     /*
